aee002a7ea3b7c05420814b1f7ad76dfa51e6e66
bilal-inksmith <109742973+mbakhtar@users.noreply.github.com>
2023-02-16 08:30:00 -0500

    Initial files for MakeCode project

---
+++ jacdac.ts
@@ -0,0 +1,11 @@
+// auto-generated, do not edit.
+namespace myModules {
+
+    //% fixedInstance whenUsed block="IR 2"
+    export const iR2 = new modules.ReflectedLightClient("iR2");
+
+    //% fixedInstance whenUsed block="IR 3"
+    export const iR3 = new modules.ReflectedLightClient("iR3");
+
+}
+    
---
+++ main.ts
@@ -0,0 +1,30 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (modules.reflectedLight1.brightness() == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+    }
+    if (myModules.iR2.brightness() == 0) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    }
+    if (myModules.iR3.brightness() == 0) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+})
